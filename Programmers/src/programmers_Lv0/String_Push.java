f76a51537085533aa1fa67acbabf72b787b3f774
qwersdfcv <qwersdfcv@qwersdfcv>
2023-04-17 12:19:14 +0900

    문자열 밀기

--- Programmers/src/programmers_Lv0/String_Push.java
+++ Programmers/src/programmers_Lv0/String_Push.java
@@ -1,5 +1,7 @@
 package programmers_Lv0;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class String_Push {
@@ -10,25 +12,26 @@ public class String_Push {
 		// 마지막 문자는 맨 앞으로 이동시키면 "ohell"이 됩니다.
 		// 이것을 문자열을 민다고 정의한다면 문자열 A와 B가 매개변수로 주어질 때,
 		// A를 밀어서 B가 될 수 있다면 밀어야 하는 최소 횟수를 return하고 밀어서
-		// B가 될 수 없으면 -1을 return 하도록 solution 함수를 완성해보세요.
+		// B가 될 수 없으면 -1을 return 하도록 함수를 완성해보세요.
 
 		Scanner scanner = new Scanner(System.in);
+		
 		char[] arrA = null;
 		char[] arrB = null;
 		String inputA, inputB;
 		char tmp;
-		int result = 0, check = 0;
+		int result = 0, check = 0;					// 변수 선언
 
 		System.out.print("문자 입력 : ");
 		inputA = scanner.nextLine();
 		System.out.println();
 		System.out.print("밀린 문자 입력 : ");
-		inputB = scanner.nextLine();
-		System.out.println();
-
+		inputB = scanner.nextLine();		
+		System.out.println();						//	입력
+		
+		Arrays.toString(arrA);		
 		arrA = inputA.toCharArray();
-		arrB = inputB.toCharArray();
-		System.out.println();
+		arrB = inputB.toCharArray();			//	배열에 저장
 
 		while (result < arrA.length) {
 			for (int i = 0; i < arrA.length; i++) {
@@ -52,7 +55,7 @@ public class String_Push {
 			System.out.printf("\"%s\"를 오른쪽으로 %s 칸 밀면 \"%s\"가 됩니다", inputA, result, inputB);
 		} else {
 			System.out.printf("\"%s\"은 몇 번을 밀어도 \"%s\"가 될 수 없습니다.", inputA, inputB);
-		}
+		}											//	출력
 		scanner.close();
 	}
 }
